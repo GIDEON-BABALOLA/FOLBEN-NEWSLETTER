30be49f780dd1613353f8dbce3d35e60e4315c53
GIDEON BABALOLA <gideonbabalola69@gmail.com>
2023-07-13 21:39:13 +0100

    new module did not work

--- Newsletter-Signup.js
+++ Newsletter-Signup.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const keyman = require(__dirname + "/API Keys.js");
-const mail = require(__dirname + "/mail.js");
 const { subscribe } = require("diagnostics_channel");
 const app = express();
 app.use(express.static("public"));
